51a542f407c8942dcf4df3bd6352f09bbd6b9d53
Camron Godbout <camron248@gmail.com>
2015-08-05 10:11:32 -0400

    Initial Commit

---
+++ tagalong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tagalong
+//
+//  Created by Camron Godbout on 8/5/15.
+//  Copyright (c) 2015 Camron Godbout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
